808bb8db078558a099273ab4cd2bb6231abc70a6
Fourfingerz <chansd5@gmail.com>
2021-07-14 22:26:02 -0400

    Modified setup.sh with some seed data and testing

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 # localstack running on docker-compose
-docker-compose up 
+docker-compose up --force-recreate --bulid -data
+terraform init && terraform apply -auto-approve
+cd static && awslocal dynamodb batch-write-item --request-items file://seed-data-dyno-format.json && cd ..
+awslocal dynamodb scan --table-name parsley # verify
+
+# testing IAM enforcement strategy
+awslocal iam create-user --user-name localstack-tester
+awslocal iam create-access-key --user-name --user-name localstack-tester
